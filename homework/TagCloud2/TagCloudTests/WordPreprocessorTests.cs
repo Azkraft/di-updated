c10be79fcd5ee113e6e7804ca1897952551a1dee
Azkraft <azanov-2004@mail.ru>
2025-12-31 18:08:14 +0500

    add(PreprocessorTests): create simple tests

--- homework/TagCloud2/TagCloudTests/WordPreprocessorTests.cs
+++ homework/TagCloud2/TagCloudTests/WordPreprocessorTests.cs
@@ -5,7 +5,24 @@ namespace TagCloudTests;
 public class WordPreprocessorTests
 {
 	[Test]
-	public void Test1()
+	public void Should_ReturnSameWordCountWithoutFiltering()
 	{
+		var preprocessor = new WordPreprocessor(new([]));
+		var words = new List<string> { "мама", "мыла", "раму" };
+
+		var output = preprocessor.Process(words);
+
+		Assert.That(output, Has.Count.EqualTo(words.Count));
+	}
+
+	[Test]
+	public void Should_FilterWords()
+	{
+		var preprocessor = new WordPreprocessor(new([PartOfSpeech.Noun]));
+		var words = new List<string> { "мама", "мыла", "раму" };
+
+		var output = preprocessor.Process(words);
+
+		Assert.That(output, Has.Count.EqualTo(0));
 	}
 }
